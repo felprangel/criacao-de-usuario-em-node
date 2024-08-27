689bda5db44a48ba244be70234148d59b821ae68
felprangel <feliperangel897@gmail.com>
2024-08-27 20:49:17 -0300

    adiciona parametros pra função

--- storages/usersStorage.js
+++ storages/usersStorage.js
@@ -5,7 +5,7 @@ class UsersStorage {
     this.id = 0;
   }
 
-  addUser({ firstName, lastName, email }) {
+  addUser({ firstName, lastName, email, age, bio }) {
     const id = this.id;
     this.storage[id] = { id, firstName, lastName, email, age, bio };
     this.id++;
